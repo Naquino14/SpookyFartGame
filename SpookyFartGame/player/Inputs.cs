871ca48793d84b6d390b29c6b2e2a145aff2473d
Nathaniel Aquino <naquino14@outlook.com>
2022-10-22 15:15:16 -0400

    humans

---
+++ SpookyFartGame/player/Inputs.cs
@@ -0,0 +1,44 @@
+﻿using System                                                                        ;
+using System.Collections.Generic                                                    ;
+using System.Linq                                                                   ;
+using System.Text                                                                   ;
+using System.Threading.Tasks                                                        ;
+using Microsoft.Xna.Framework                                                       ;
+using Microsoft.Xna.Framework.Input                                                 ;
+
+namespace SpookyFartGame.player{public enum Direction{
+   center,up,down,left,right,upRight,upLeft,downRight,downLeft}
+    
+            public static class Inputs {
+        public static Direction 
+GetPlayerState(PlayerIndex index)
+        {
+            bool up = GamePad.GetState(index).DPad.Up == ButtonState.Pressed        ,
+            down = GamePad.GetState(index).DPad.Down == ButtonState.Pressed         ,
+            left = GamePad.GetState(index).DPad.Left == ButtonState.Pressed         ,
+            right = GamePad.GetState(index).DPad.Right == ButtonState.Pressed       ;
+
+        if (
+up && down 
+                || left && 
+right)
+            return Direction.center                                                 ;
+                else if (up && right)
+            return Direction.upRight                                                ;
+                else if (up && left)
+            return Direction.upLeft                                                 ;
+            else if (down && right)
+            return Direction.downRight                                              ;
+            else if (down && left)
+                return Direction.downLeft                                           ;
+                else if (up)
+            return Direction.up                                                     ;
+             else if (down)
+                return Direction.down                                               ;
+            else if (left)  
+                return Direction.left                                               ;
+            else if (right)
+           return Direction.right                                                   ;
+            else
+            return Direction.center                                                 ;
+}}}
